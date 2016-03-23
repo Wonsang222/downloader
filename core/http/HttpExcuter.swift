3e6e20eb0ec3a30c70dde030aec6f707b17b4459
pascman <pascman@nate.com>
2016-03-23 20:20:52 +0900

    Commit

---
+++ core/http/HttpExcuter.swift
@@ -0,0 +1,17 @@
+//
+//  HttpExcuter.swift
+//  magicapp
+//
+//  Created by JooDaeho on 2016. 3. 20..
+//  Copyright © 2016년 JooDaeho. All rights reserved.
+//
+
+import Foundation
+
+class HttpExcuter{
+    
+    func sendRequest(){
+        
+    }
+    
+}

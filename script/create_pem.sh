d5bd2cbd90454f965ee015e26ccee0229570eff0
RedSea <pascman@nate.com>
2016-06-17 10:46:13 +0900

    sss

---
+++ script/create_pem.sh
@@ -0,0 +1,8 @@
+openssl pkcs12 -in cert.p12 -out cert.pem -nodes
+openssl pkcs12 -in key.p12 -out key.pem -nodes
+openssl rsa -in key.pem -out key.unencrypted.pem
+cat cert.pem key.unencrypted.pem > apns.pem
+rm cert.pem
+rm key.pem
+rm key.unencrypted.pem
+

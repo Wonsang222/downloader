b099c0f4d8a8deb456dcd9bde9a3b851ef5554d7
dongchul <choise154@gmail.com>
2018-01-19 14:38:12 +0900

    subView 수정

--- core/WebController/MGWKSubWebController.swift
+++ core/WebController/MGWKSubWebController.swift
@@ -8,6 +8,8 @@
 import WebKit
 import UIKit
 
+
+
 class MGWKSubWebController: BaseController, WKUIDelegate  {
     
     var startUrl:String?
@@ -20,43 +22,67 @@ class MGWKSubWebController: BaseController, WKUIDelegate  {
     var webTitle: UILabel!
     // title url
     var webUrl: UILabel!
+    // status bar
+    var statusBar: UIView!
+    // bottom border
+    var bottomBorder: UIView!
     
     func loadedView(url: URLRequest, config: WKWebViewConfiguration) -> WKWebView {
         webViewSubContainer = UIView()
         topNavigationView = UIView()
         webTitle = UILabel()
         webUrl = UILabel()
-
-        let dismissBtn = UIButton()
+        statusBar = UIView()
+        bottomBorder = UIView()
         
+        let dismissBtn: UIButton = UIButton()
+        let locationbar_height: CGFloat = 50.0
+        
+        print("statusbar : \(UIApplication.shared.statusBarFrame.height)")
         webView = WKWebView(frame: UIScreen.main.bounds, configuration: config)
         
         if #available(iOS 9.0, *) {
             //            webView.allowsLinkPreview = true
         }
         self.view.bounds = UIScreen.main.bounds
-        webViewSubContainer.bounds = self.view.bounds
+        
+        statusBar.frame = CGRect(x: 0.0, y: 0.0, width: self.view.bounds.width, height: UIApplication.shared.statusBarFrame.height)
+        statusBar.backgroundColor = .white
+        webViewSubContainer.backgroundColor = .red
+        webViewSubContainer.frame = CGRect(x: 0.0, y: UIApplication.shared.statusBarFrame.height, width: self.view.bounds.width, height: self.view.bounds.height)
+        
+        self.view.addSubview(statusBar)
         self.view.addSubview(webViewSubContainer)
         
-        webViewSubContainer.center = self.view.center
+//        webViewSubContainer.center = self.view.center
         
-        topNavigationView.frame = CGRect(x: 0.0, y: UIApplication.shared.statusBarFrame.height - 20, width: webViewSubContainer.bounds.width, height: 80)
+        topNavigationView.frame = CGRect(x: 0.0, y:0.0, width: webViewSubContainer.bounds.width, height: locationbar_height)
         topNavigationView.backgroundColor = UIColor.white
-
+        
+        bottomBorder.frame = CGRect(x: 0, y: locationbar_height, width: webViewSubContainer.bounds.width, height: 1.0)
+        bottomBorder.backgroundColor = UIColor(hexString: "#c9c9c9")
+        
+        
         topNavigationView.addSubview(dismissBtn)
-        dismissBtn.frame = CGRect(x: 5.0, y: 12.5, width: 65.0, height: 65.0)
-        dismissBtn.contentEdgeInsets.top = 10.0
+        topNavigationView.addSubview(bottomBorder)
+        dismissBtn.frame = CGRect(x: 5.0, y: 0.0, width: topNavigationView.bounds.height, height: topNavigationView.bounds.height)
+        
         dismissBtn.setImage(UIImage(named: "ic_navi_back.png"), for: .normal)
         dismissBtn.onClick { (view) in
             self.dismiss(animated: true, completion: nil)
         }
-        webTitle.frame = CGRect(x: 0.0, y: 30.0, width: topNavigationView.bounds.width, height: 20.0)
-        webUrl.frame = CGRect(x: 0.0, y: 50.0, width: topNavigationView.bounds.width, height: 20.0)
-        webTitle.font.withSize(12.0)
-        webUrl.font.withSize(10.0)
+//        dismissBtn.backgroundColor = .red
+//        webTitle.backgroundColor = .blue
+//        webTitle.backgroundColor = .green
+        webTitle.frame = CGRect(x: 0.0, y: topNavigationView.center.y - 18, width: topNavigationView.bounds.width, height: 20.0)
+        webUrl.frame = CGRect(x: 0.0, y: topNavigationView.center.y - 3, width: topNavigationView.bounds.width, height: 20.0)
+        
+        webTitle.font = webTitle.font.withSize(12.0)
+        webUrl.font = webUrl.font.withSize(10.0)
+        
         webTitle.textColor = UIColor(hexString: "#333333")
-
         webUrl.textColor = UIColor(hexString: "#888888")
+        
         webTitle.textAlignment = .center
         webUrl.textAlignment = .center
         
@@ -64,7 +90,8 @@ class MGWKSubWebController: BaseController, WKUIDelegate  {
             webTitle.text = "로그인"
         } else if webView.title!.isEmpty {
             webTitle.text = ""
-            webUrl.frame.origin.y = topNavigationView.center.y
+            webUrl.font = webUrl.font.withSize(12.0)
+            webUrl.frame.origin.y = topNavigationView.center.y - 10
         } else {
             webTitle.text = webView.title!
         }
@@ -75,9 +102,9 @@ class MGWKSubWebController: BaseController, WKUIDelegate  {
         topNavigationView.addSubview(webUrl)
         
         webView.addSubview(topNavigationView)
-        webView.scrollView.contentInset = UIEdgeInsetsMake(topNavigationView.bounds.height - 20, 0.0, 0.0, 0.0)
-        webView.bounds = webViewSubContainer.bounds
-        webView.bounds = CGRect(x: 0.0, y: 0.0, width: webViewSubContainer.bounds.width, height: webViewSubContainer.bounds.height)
+        webView.scrollView.contentInset = UIEdgeInsetsMake(topNavigationView.bounds.height, 0.0, 0.0, 0.0)
+        webView.frame = webViewSubContainer.bounds
+        webView.frame = CGRect(x: 0.0, y: 0.0, width: webViewSubContainer.bounds.width, height: webViewSubContainer.bounds.height)
         webView.uiDelegate = self
         webView.navigationDelegate = self as? WKNavigationDelegate
         webView.load(url)

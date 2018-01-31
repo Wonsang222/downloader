cff7851d9671381a7bf1fd1bf8e16479ee7e3e13
dongchul <choise154@gmail.com>
2018-01-31 15:30:23 +0900

    알림함 클릭 이슈

--- core/WNotiController.swift
+++ core/WNotiController.swift
@@ -24,10 +24,11 @@ class WNotiController: BaseWebController,UIScrollViewDelegate,WebControlDelegate
         self.engine.webDelegate = self
         ThemeFactory.createTheme(self, themeInfo: WInfo.themeInfo)?.applyNavi()
         self.engine.webView.alpha = 0
-        var url = URL (string: HttpMap.PUSH_PAGE + "?account_id=" + WInfo.accountId + "&view=" + type!) ;
-        if link != nil {
-        	url = URL (string: link!);
-        }
+        type = type != nil ? type : "all"
+        let url = URL (string: HttpMap.PUSH_PAGE + "?account_id=" + WInfo.accountId + "&view=" + type!) ;
+//        if link != nil {
+//            url = URL (string: link!);
+//        }
         var requestObj = URLRequest(url: url!);
         if let userId = WInfo.userInfo["userId"] as? String{
             requestObj.addValue(userId.encryptAES256(), forHTTPHeaderField: "MAGIC_USER_ID")

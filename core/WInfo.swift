27ba588dfdcc991d9e47667e63652ddeabd42903
WISA <wisa.dev@gmail.com>
2018-09-12 16:25:31 +0900

    임시 커밋. 롤백 요망

--- core/WInfo.swift
+++ core/WInfo.swift
@@ -371,7 +371,7 @@ class WInfo{
     static var getAbleTheme: [String] {
         get {
             return ["T1","T2","T3",
-//                    "T4TEST"
+                    "T4TEST"
 //                나인
 //                ,"CNAIN"
             ]

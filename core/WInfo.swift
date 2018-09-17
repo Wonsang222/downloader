19b83038449d76dc2dc5d2d47abb8919916c0a88
WISA <wisa.dev@gmail.com>
2018-09-17 16:06:08 +0900

    원복 후 테스트

--- core/WInfo.swift
+++ core/WInfo.swift
@@ -370,7 +370,7 @@ class WInfo{
     // ThemeInfo
     static var getAbleTheme: [String] {
         get {
-            return ["T1","T2","T3", 
+            return ["T1","T2","T3","T4",
 //                나인
 //                ,"CNAIN"
             ]

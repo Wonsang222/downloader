90380e34441f3b86a5c55e8309f4c9c59e44016e
wisa <wisa.dev@gmail.com>
2019-06-19 17:10:14 +0900

    pod 머지

---
+++ core/ModalViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ModalViewController.swift
+//  wing
+//
+//  Created by khh on 21/12/2018.
+//  Copyright © 2018 JooDaeho. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onModalShow() {
+        
+    }
+}
---
+++ core/TestDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  TestDelegate.swift
+//  wing
+//
+//  Created by khh on 17/12/2018.
+//  Copyright © 2018 JooDaeho. All rights reserved.
+//
+
+import UIKit
+
+class TestDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    
+}

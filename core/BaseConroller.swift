4d32781e6205cd8cf148b9c719e5799be01f10bd
이동철 <choise154@gmail.com>
2020-03-17 15:09:41 +0900

    - iOS Target Sdk 9.2 -> 13.0 으로 변경 - 모든 모달 fullscreen 으로 변경

--- core/BaseConroller.swift
+++ core/BaseConroller.swift
@@ -16,6 +16,7 @@ class BaseController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.modalPresentationStyle = .fullScreen
     }
     
     override func viewWillAppear(_ animated: Bool) {

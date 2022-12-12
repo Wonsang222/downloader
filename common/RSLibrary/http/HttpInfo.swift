c8264decae0b0d3d137388b0057005b286c444a7
Dongd <choise154@gmail.com>
2022-12-12 14:17:35 +0900

    매직앱 서버(admin.miagicapp.co.kr) http -> https 로 변경

--- common/RSLibrary/http/HttpInfo.swift
+++ common/RSLibrary/http/HttpInfo.swift
@@ -1,5 +1,6 @@
 struct HttpInfo{
-	static let HOST = "http://admin.magicapp.co.kr"
+    // 2022.12.12 http -> https 변경
+    static let HOST = "https://admin.magicapp.co.kr"
 //    static let HOST = "http://118.129.243.248:8109"
 
 	static let TIMEOUT = 15000
